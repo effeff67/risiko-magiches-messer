5f2ed04acbe5c570fab8ad6b1b79a80547c6fd8d
frfr <frfr@spreadshirt.net>
2019-08-02 09:30:38 +0200

    starting vue

---
+++ client/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="app">
+        <GameMap/>
+        <PlayerMission/>
+        <PlayerActionConsole/>
+        <PlayerCardStack/>
+        <PlayerFiguresStack/>
+    </div>
+</template>
+
+<script>
+    import GameMap from './components/GameMap.vue'
+    import PlayerMission from "./components/PlayerMission";
+    import PlayerActionConsole from "./components/PlayerActionConsole";
+    import PlayerCardStack from "./components/PlayerCardStack";
+    import PlayerFiguresStack from "./components/PlayerFigureStack";
+
+    export default {
+        name: 'app',
+        components: {
+            PlayerFiguresStack,
+            PlayerCardStack,
+            PlayerActionConsole,
+            PlayerMission,
+            GameMap
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    #gameMap, #playerMission {
+        position: absolute;
+        width: 80%;
+        left: 0;
+    }
+
+    #gameMap {
+        top: 0;
+        height: 95%;
+        background-color: #2c3e50;
+    }
+
+    #playerMission {
+        bottom: 0;
+        height: 5%;
+        background-color: brown;
+    }
+
+    #playerActionConsole, #playerFigureStack, #playerCardStack {
+        position: absolute;
+        left: 80%;
+        width: 20%;
+    }
+
+    #playerActionConsole {
+        top:0;
+        height: 60%;
+        background-color: beige;
+    }
+
+    #playerFigureStack {
+        bottom:0;
+        height: 10%;
+        background-color: white;
+    }
+
+    #playerCardStack {
+        top:60%;
+        height: 30%;
+        background-color: red;
+    }
+</style>
---
+++ client/src/components/PlayerFigureStack.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="playerFigureStack">
+        <span id="activatedFigures">{{ countActivatedFigures }}</span>
+        <span id="inactiveFigures">{{ countInactiveFigures }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PlayerFiguresStack",
+        data: function() {
+            return {
+                countActivatedFigures: 0,
+                countInactiveFigures: 35,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #activatedFigures, #inactiveFigures {
+        display: block;
+        float:left;
+        margin: 2%;
+        height: 2%;
+        padding: 4%;
+        border-radius: 20%;
+        border: 1px solid black;
+        background-color: white;
+    }
+
+    #activatedFigures {
+        color: black;
+    }
+
+    #inactiveFigures {
+        color:lightgrey;
+    }
+</style>
