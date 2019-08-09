a938a7820df26723c5eb63c75671c45c7a9cfa45
frfr <frfr@spreadshirt.net>
2019-08-09 18:06:31 +0200

    implements addPlayer validator and executor

--- server/src/main/java/edu/htwk/mm/risiko/model/Player.java
+++ server/src/main/java/edu/htwk/mm/risiko/model/Player.java
@@ -3,8 +3,6 @@ package edu.htwk.mm.risiko.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class Player {
--- server/src/main/java/edu/htwk/mm/risiko/service/validation/AddPlayerValidator.java
+++ server/src/main/java/edu/htwk/mm/risiko/service/validation/AddPlayerValidator.java
@@ -1,10 +1,12 @@
 package edu.htwk.mm.risiko.service.validation;
 
 import edu.htwk.mm.risiko.model.Game;
+import edu.htwk.mm.risiko.model.Status;
 import edu.htwk.mm.risiko.model.api.GameChangeResponse;
 import edu.htwk.mm.risiko.model.api.GameCommandRequest;
 import edu.htwk.mm.risiko.service.execution.AddPlayerExec;
 import edu.htwk.mm.risiko.service.execution.CommandExecutor;
+import edu.htwk.mm.risiko.service.execution.InvalidCommandExec;
 
 public class AddPlayerValidator implements CommandValidator {
 
@@ -13,12 +15,26 @@ public class AddPlayerValidator implements CommandValidator {
 
     public AddPlayerValidator(GameCommandRequest command) {
         this.command = command;
-        this.response =new GameChangeResponse();
+        this.response = new GameChangeResponse();
     }
 
     @Override
     public CommandExecutor validate(Game game) {
-
+        response.setStatus(Status.ERROR);
+        if(game.getPlayers().stream()
+                .anyMatch(player -> player.getColor() == command.getPlayer().getColor() )) {
+            response.setMessage("Diese Farbe ist bereits vergeben.");
+            return new InvalidCommandExec(response);
+        }
+        if(game.isStarted()) {
+            response.setMessage("Dieses Spiel ist bereits gestartet.");
+            return new InvalidCommandExec(response);
+        }
+        if(game.getPlayers().size() >= 5) {
+            response.setMessage("Dieses Spiel hat bereits genügend Mitspieler.");
+            return new InvalidCommandExec(response);
+        }
+        response.setStatus(Status.SUCCESS);
         return new AddPlayerExec(game, command, response);
     }
 
