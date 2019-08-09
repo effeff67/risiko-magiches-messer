6db07a3c6c7e295d971cbab53f4fbdce1448d504
frfr <frfr@spreadshirt.net>
2019-08-09 18:23:31 +0200

    fix constructor

--- server/src/main/java/edu/htwk/mm/risiko/service/validation/InvalidCommandValidation.java
+++ server/src/main/java/edu/htwk/mm/risiko/service/validation/InvalidCommandValidation.java
@@ -1,12 +1,14 @@
 package edu.htwk.mm.risiko.service.validation;
 
 import edu.htwk.mm.risiko.model.Game;
+import edu.htwk.mm.risiko.model.Status;
+import edu.htwk.mm.risiko.model.api.GameChangeResponse;
 import edu.htwk.mm.risiko.service.execution.CommandExecutor;
 import edu.htwk.mm.risiko.service.execution.InvalidCommandExec;
 
 public class InvalidCommandValidation implements CommandValidator {
     @Override
     public CommandExecutor validate(Game game) {
-        return new InvalidCommandExec();
+        return new InvalidCommandExec(new GameChangeResponse(Status.ERROR, "invalid Command"));
     }
 }
