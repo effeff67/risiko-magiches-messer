1c2acea00150472da69753cb1dce6516161111e3
Erik2101 <erik.semmler@web.de>
2019-08-11 15:00:35 +0200

    edited DrawCardValidator

--- server/src/main/java/edu/htwk/mm/risiko/service/validation/DrawCardValidator.java
+++ server/src/main/java/edu/htwk/mm/risiko/service/validation/DrawCardValidator.java
@@ -2,12 +2,14 @@ package edu.htwk.mm.risiko.service.validation;
 
 import com.sun.net.httpserver.Authenticator;
 import edu.htwk.mm.risiko.model.Game;
+import edu.htwk.mm.risiko.model.Player;
 import edu.htwk.mm.risiko.model.Status;
 import edu.htwk.mm.risiko.model.api.GameChangeRequest;
 import edu.htwk.mm.risiko.model.api.GameChangeResponse;
 import edu.htwk.mm.risiko.service.execution.CommandExecutor;
 import edu.htwk.mm.risiko.service.execution.DrawCardExec;
 import edu.htwk.mm.risiko.service.execution.InvalidCommandExec;
+import edu.htwk.mm.risiko.service.util.GameEntityFinder;
 
 public class DrawCardValidator implements CommandValidator {
 
@@ -16,18 +18,16 @@ public class DrawCardValidator implements CommandValidator {
 
     public DrawCardValidator(GameChangeRequest command) {
         this.command = command;
-        this.response = new GameChangeResponse();
+        this.response = new GameChangeResponse(Status.ERROR);
     }
 
     @Override
     public CommandExecutor validate(Game game) {
-        response.setStatus(Status.ERROR);
-        if(!command.getPlayer().isConquered()){
-            response.setMessage("Sie Haben diese Kampfphase nicht erfolgreich erobert. Sie bekommen keine Karte.");
-            return new InvalidCommandExec(response);
+        Player player = GameEntityFinder.findPlayerByColor(game, command.getPlayer().getColor());
+        if(player.getColor() != game.getLastOccupation().getColor()){
+            return new InvalidCommandExec(response.setMessage("Sie Haben diese Kampfphase nicht erfolgreich erobert. Sie bekommen keine Karte."));
         }
         response.setStatus(Status.SUCCESS);
-        response.setMessage("Sie haben erfolreich erobert, Sie bekommen eine Karte.");
-        return new DrawCardExec(game, command, response);
+        return new DrawCardExec(game, command, response.setMessage("Sie haben erfolreich erobert, Sie bekommen eine Karte."));
     }
 }
