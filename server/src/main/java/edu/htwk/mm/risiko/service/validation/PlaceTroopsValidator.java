2be2662a75dd520b1abefc39b6333267d1ff1ea9
Erik2101 <erik.semmler@web.de>
2019-08-11 14:41:26 +0200

    finished PlaceTroopsValidator

--- server/src/main/java/edu/htwk/mm/risiko/service/validation/PlaceTroopsValidator.java
+++ server/src/main/java/edu/htwk/mm/risiko/service/validation/PlaceTroopsValidator.java
@@ -2,14 +2,14 @@ package edu.htwk.mm.risiko.service.validation;
 
 import edu.htwk.mm.risiko.model.Country;
 import edu.htwk.mm.risiko.model.Game;
+import edu.htwk.mm.risiko.model.Player;
 import edu.htwk.mm.risiko.model.Status;
 import edu.htwk.mm.risiko.model.api.GameChangeRequest;
 import edu.htwk.mm.risiko.model.api.GameChangeResponse;
-import edu.htwk.mm.risiko.model.api.GameCommand;
-import edu.htwk.mm.risiko.model.api.GameCommandRequest;
 import edu.htwk.mm.risiko.service.execution.CommandExecutor;
 import edu.htwk.mm.risiko.service.execution.InvalidCommandExec;
 import edu.htwk.mm.risiko.service.execution.PlaceTroopsExec;
+import edu.htwk.mm.risiko.service.util.GameEntityFinder;
 
 public class PlaceTroopsValidator implements CommandValidator {
 
@@ -24,16 +24,19 @@ public class PlaceTroopsValidator implements CommandValidator {
     @Override
     public CommandExecutor validate(Game game) {
         response.setStatus(Status.ERROR);
-     if(command.getCommandDetails().get(country).getHolder() == command.getPlayer().getColor()) {
-         response.setMessage("Du kontrollierst das gewählte Land nicht.");
-         return new InvalidCommandExec(response);
-     }
-     if(command.getCommandDetails().get(troops) > command.getPlayer().getInactiveTroops()){
-         response.setMessage("Du besitzt nicht genügend Truppen.");
-         return new InvalidCommandExec(response);
-     }
-        response.setStatus(Status.SUCCESS);
-     response.setMessage("Es wurden " + " Truppen auf " + " platziert.");
-     return new PlaceTroopsExec(game, command, response);
+        Object countryName = command.getCommandDetails().get("country");
+        Integer troopCount = (Integer) command.getCommandDetails().get("troopCount");
+        Country country = GameEntityFinder.findCountryByName(game.getGameMap(), countryName.toString());
+            if (country.getHolder() == command.getPlayer().getColor()) {
+                response.setMessage("Du kontrollierst das gewählte Land nicht.");
+                return new InvalidCommandExec(response);
+            }
+            if (troopCount > command.getPlayer().getInactiveTroops()) {
+                response.setMessage("Du besitzt nicht genügend Truppen.");
+                return new InvalidCommandExec(response);
+            }
+            response.setStatus(Status.SUCCESS);
+            Player player = command.getPlayer();
+            return new PlaceTroopsExec(game, response, player, country, troopCount);
     }
 }
